4d01a96930bbd49d5d51b7e6aabd5ed706229b44
Jason E. Aten <j.e.aten@gmail.com>
2026-05-31 21:46:20 -0500

    print the Load cfg

--- check.go
+++ check.go
@@ -161,6 +161,7 @@ func (gp *GownPackage) AnalyzeWithOptions(opts CheckOptions) (*GownAnalysis, err
 		_, statErr := os.Stat(path)
 		vv("GOWN packages.Load overlay path=%q bytes=%d diskExists=%v statErr=%v", path, len(src), statErr == nil, statErr) // check.go:162 [goID 1] 2026-06-01 02:31:38.196248938 +0000 UTC GOWN packages.Load overlay path="/home/jaten/go/src/github.com/glycerine/gown/vectors/linkedlist00/main.go" bytes=3089 diskExists=false statErr=stat /home/jaten/go/src/github.com/glycerine/gown/vectors/linkedlist00/main.go: no such file or directory
 	}
+	vv(`about to packages.Load(cfg, ".") with cfg = '%#v'`, cfg)
 	pkgs, err := packages.Load(cfg, ".")
 	if err != nil {
 		vv("GOWN AnalyzeWithOptions return: packages.Load failed dir=%q err=%v", cfg.Dir, err)
